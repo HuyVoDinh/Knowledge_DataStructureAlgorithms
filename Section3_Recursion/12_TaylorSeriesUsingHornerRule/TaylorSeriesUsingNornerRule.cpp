548338f1464a47c063f6a2d6ae20a93d80ed7e81
HuyVoDinh <huy.vd76@gmail.com>
2025-02-02 10:45:01 +0700

    [Section 3][Update] Update taylor using loop

--- Section3_Recursion/12_TaylorSeriesUsingHornerRule/TaylorSeriesUsingNornerRule.cpp
+++ Section3_Recursion/12_TaylorSeriesUsingHornerRule/TaylorSeriesUsingNornerRule.cpp
@@ -9,6 +9,20 @@ double e(int x, int n)
     return e(x,n-1);
 }
 
+double e_loop(int x, int n)
+{
+    double result = 1;
+    double num=1;
+    double den=1;
+    for(int i = 0; i <= n; i++)
+    {
+        num*=x;
+        den*=i;
+        result+=num/den;
+    }
+    return result;
+}
+
 int main()
 {
     printf("%lf\n", e(2,10));
