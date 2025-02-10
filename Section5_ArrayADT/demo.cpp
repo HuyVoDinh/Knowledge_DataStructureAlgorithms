51a6b91b0f60e32be53050a44e1d67f821c3250b
HuyVoDinh <huy.vd76@gmail.com>
2025-02-11 00:22:02 +0700

    [Section 5][Add] Binary search

--- Section5_ArrayADT/demo.cpp
+++ Section5_ArrayADT/demo.cpp
@@ -10,7 +10,7 @@ struct Array
 void Display(struct Array arr)
 {
     int i;
-    printf("Elements are\n");
+    printf("\nElements are\n");
     for(int i = 0; i < arr.length; i++)
         printf("%d ", arr.A[i]);
 }
@@ -74,6 +74,39 @@ int LinearSearch(struct Array arr, int key)
     return -1;
 }
 
+int BinarySearch(struct Array arr, int key)
+{
+    int l, mid, h;
+    l = 0; h = arr.length-1;
+    while(l <= h)
+    {
+        mid = (l+h)/2;
+        if(key==arr.A[mid])
+            return mid;
+        else if(key < arr.A[mid])
+            h = mid-1;
+        else 
+            l = mid+1;
+    }
+    return -1;
+}
+
+int RBinSearch(int a[], int l, int h, int key)
+{
+    int mid;
+    if(l <= h)
+    {
+        mid = (l+h)/2;
+        if(key == a[mid])
+            return mid;
+        else if (key < a[mid])
+            return RBinSearch(a,l,mid-1,key);
+        else
+            return RBinSearch(a,mid+1,h, key);
+    }
+    return -1;
+}
+
 int main()
 {
     struct Array arr = {{2,3,4,5,6},20,5};
@@ -98,8 +131,10 @@ int main()
     Insert(&arr,2,30);
     Display(arr);
     printf("%d\n",Delete(&arr,4));
-    printf("%d\n",LinearSearch(arr,4));
-   
+    Display(arr);
+    printf("\nLinear search: %d\n",LinearSearch(arr,4));
+    printf("\nBinary search: %d\n",BinarySearch(arr,3));
+    printf("\nRBin search: %d\n",RBinSearch(arr.A,0,arr.length-1,2));
 
     return 0; 
 }
