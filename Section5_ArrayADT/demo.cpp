23d33d305339aa2906d5611bcf3e02c876fa0198
HuyVoDinh <huy.vd76@gmail.com>
2025-02-12 21:14:44 +0700

    [Section 5][Add] Add get,set, max, min, sum function

--- Section5_ArrayADT/demo.cpp
+++ Section5_ArrayADT/demo.cpp
@@ -107,6 +107,55 @@ int RBinSearch(int a[], int l, int h, int key)
     return -1;
 }
 
+int Get(struct Array arr, int index)
+{
+    if (index >= 0 && index<arr.length)
+        return arr.A[index];
+    return -1;
+}
+
+void Set(struct Array *arr,int index, int x)
+{
+    if(index >= 0 && index <arr->length)
+        arr->A[index] = x;
+}
+
+int Max(struct Array arr)
+{
+    int max = arr.A[0];
+    for(int i = 1; i < arr.length; i++)
+    {
+        if(arr.A[i] > max)
+            max = arr.A[i];
+    }
+    return max;
+}
+
+int Min(struct Array arr)
+{
+    int min = arr.A[0];
+    for(int i = 1; i < arr.length; i++)
+    {
+        if(arr.A[i] > min)
+            min = arr.A[i];
+    }
+    return min;
+}
+
+int Sum (struct Array arr)
+{
+    int s = 0;
+    for(int i = 0; i < arr.length;i++)
+        s += arr.A[i];
+
+    return s;
+}
+
+float Avg(struct Array arr)
+{
+    return (float)Sum(arr)/arr.length;
+}
+
 int main()
 {
     struct Array arr = {{2,3,4,5,6},20,5};
@@ -135,6 +184,13 @@ int main()
     printf("\nLinear search: %d\n",LinearSearch(arr,4));
     printf("\nBinary search: %d\n",BinarySearch(arr,3));
     printf("\nRBin search: %d\n",RBinSearch(arr.A,0,arr.length-1,2));
+    printf("Get: %d\n", Get(arr,9));
+    Set(&arr,0,15);
+    Display(arr);
+    printf("Max: %d\n", Max(arr));
+    printf("Min: %d\n", Min(arr));
+    printf("Sum: %d\n", Sum(arr));
+    printf("Avg: %f\n", Avg(arr));
 
     return 0; 
 }
