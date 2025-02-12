c5f22e900babee987dc8da491dc68d7bb8483067
HuyVoDinh <huy.vd76@gmail.com>
2025-02-12 22:33:01 +0700

    [Section 5][Add] Insert sort, is sorted, Rearrange function

--- Section5_ArrayADT/demo.cpp
+++ Section5_ArrayADT/demo.cpp
@@ -173,6 +173,44 @@ void Reverse2(struct Array *arr)
     }
 }
 
+void InsertSort(struct Array *arr, int x)
+{
+    if(arr->length==arr->size)
+        return;
+    int i = arr->length-1;
+    while(i <= 0 && arr->A[i]>x)
+    {
+        arr->A[i+1] = arr->A[i];
+        i--;
+    }
+    arr->A[i+1] = x;
+    arr->length++;
+}
+
+int isSorted(struct Array arr)
+{
+    for(int i = 0; i < arr.length-1;i++)
+    {
+        if(arr.A[i]>arr.A[i+1])
+            return 0;
+    }
+    return 1;
+}
+
+void Rearrange(struct Array *arr)
+{
+    int i,j;
+    i = 0;
+    j = arr->length-1;
+    while(i<j)
+    {
+        while(arr->A[i]<0) i++;
+        while(arr->A[j]>=0)j--;
+        if(i<j)
+            swap(&arr->A[i], &arr->A[j]);
+    }
+}
+
 int main()
 {
     struct Array arr = {{2,3,4,5,6},20,5};
@@ -215,6 +253,16 @@ int main()
 
     Display(arr);
     Reverse2(&arr);
+    Display(arr);
+
+    arr = {{2,3,4,5,6},20,5};
+    InsertSort(&arr,20);
+    Display(arr);
+
+    printf("\nIs sorted: %d\n", isSorted(arr));
+    arr = {{2,3,-4,-5,6},20,5};
+    Display(arr);
+    Rearrange(&arr);
     Display(arr);
 
     return 0; 
