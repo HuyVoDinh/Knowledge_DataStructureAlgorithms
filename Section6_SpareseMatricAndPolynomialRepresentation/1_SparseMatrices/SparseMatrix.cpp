c56060bc68768718b275724ec236d51c5d3e6d48
HuyVoDinh <huy.vd76@gmail.com>
2025-02-23 18:00:21 +0700

    [Section 6][Add] Add 2 matrix

--- Section6_SpareseMatricAndPolynomialRepresentation/1_SparseMatrices/SparseMatrix.cpp
+++ Section6_SpareseMatricAndPolynomialRepresentation/1_SparseMatrices/SparseMatrix.cpp
@@ -17,9 +17,9 @@ struct Sparse
 
 void create(struct Sparse *s)
 {
-    printf("Enter Dimensions");
+    printf("Enter Dimensions: ");
     scanf("%d%d", &s->m, &s->n);
-    printf("Number of non-zero");
+    printf("Number of non-zero: ");
     scanf("%d", &s->num);
 
     s->ele = new Element[s->num];
@@ -48,11 +48,57 @@ void display(struct Sparse s)
     }
 }
 
+struct Sparse *add(Sparse *s1, Sparse *s2)
+{
+    struct Sparse *sum;
+
+    sum = new Sparse[sizeof(Sparse)];
+    sum->ele = new Element[s1->num + s2->num];
+    int i,j,k;
+    i = j = k = 0;
+    while(i<s1->num && j<s2->num)
+    {
+        if(s1->ele[i].i < s2->ele[j].i)
+            sum->ele[k++] = s1->ele[i++];
+        else if(s1->ele[i].i > s2->ele[j].i)
+            sum->ele[k++] = s2->ele[j++];
+        else
+        {
+            if(s1->ele[i].j < s2->ele[j].j)
+                sum->ele[k++] = s1->ele[i++];
+            else if (s1->ele[i].j > s2->ele[j].j)
+                sum->ele[k++] = s2->ele[j++];
+            else
+            {
+                sum->ele[k] = s1->ele[i];
+                sum->ele[k++].x = s1->ele[i++].x + s2->ele[j++].x;
+            }
+        }
+            
+    }
+    //Copy the rest of the elements from the remaining matrix. 
+    for(;i<s1->num;i++) sum->ele[k++] = s1->ele[i];
+    for(;j<s2->num;j++) sum->ele[k++] = s2->ele[j];
+    sum->m = s1->m;
+    sum->n = s1->n;
+    sum->num = k;
+
+    return sum;
+}
+
 int main()
 {
-    struct Sparse s;
+    struct Sparse s1,s2, *s3;
 
-    create (&s);
-    display(s);
+    create (&s1);
+    create (&s2);
+
+    s3 = add(&s1,&s2);
+    printf("First matrix\n");
+    display(s1);
+    printf("Second matrix\n");
+    display(s2);
+    printf("Sum matrix\n");
+    display(*s3);
     return 0;
 }
