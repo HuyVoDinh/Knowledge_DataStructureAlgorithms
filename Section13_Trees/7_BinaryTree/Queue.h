f6147209e127ac6e13da5d83b4ef264868b7ac33
HuyVoDinh <huy.vd76@gmail.com>
2025-03-30 21:23:54 +0700

    [Section 13][Add] Tree

---
+++ Section13_Trees/7_BinaryTree/Queue.h
@@ -0,0 +1,59 @@
+#ifndef Queue_h
+#define Quque_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    Node *lchild;
+    int data;
+    Node *rchild;
+};
+
+struct Queue
+{
+    int size;
+    int front;
+    int rear;
+    Node **Q;       // first point is for array, second point for type node. It's going to store pointer of type nodes. Then this queue structure.  When we are creating an array, it is not integer type pointer, it's a node type pointer to a pointer. That is double pointer.
+};
+
+void create(Queue *q, int size)
+{
+    q->size = size;
+    q->front = q->rear = 0;
+    *q->Q = new Node();
+}
+
+void enqueue(Queue *q, Node *x)
+{
+    if((q->rear+1)%q->size == q->front)
+        printf("Queue is Full");
+    else
+    {
+        q->rear = (q->rear+1)%q->size;
+        q->Q[q->rear] = x;
+    }
+}
+
+Node* dequeue(Queue *q)
+{
+    Node *x = NULL;
+    if(q->front==q->rear)
+        printf("Queue is Empty\n");
+    else
+    {
+        q->front=(q->front+1)%q->size;
+        x= q->Q[q->front];
+    }
+
+    return x;
+}
+
+int isEmpty(Queue q)
+{
+    return q.front == q.rear;
+}
+
+#endif
